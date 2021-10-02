baaecb6ca4baae7ea00dcb3cca61d374250ed2ce
hewuling001 <511873817@qq.com>
2021-10-02 17:47:59 +0800

    第二天学习：设计小游戏

--- index1.py
+++ index1.py
@@ -1,7 +1,7 @@
 print("---------我爱鱼C工作室--------")
 temp = input("不妨猜一下小甲鱼现在心里想的是哪个数字：")
 guess = input(temp)
-if guess == 8；
+if guess == 8:
     print("我草，你是小甲鱼心里的蛔虫吗？！")
     print("哼，猜中了也没有奖励！")
 else:
